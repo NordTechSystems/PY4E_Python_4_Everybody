3c56d736d69dc2b78a59544e15fe9079af3cda2c
nordtechsystems <tom.wilson@nordtechsystems.com>
2021-10-11 20:53:03 -0400

    Completed excercise_03_02 from Python for Everybody (PY4E)

---
+++ excercise_03_02/excercise_03_02.py
@@ -0,0 +1,20 @@
+enter_hours = input("Enter Hours: ")
+enter_rate = input("Enter Rate: ")
+try:
+    floatingPointHours = float(enter_hours)
+    floatingPointRate = float(enter_rate)
+except: 
+    print("Error, please enter numberic input")
+    quit()
+# print(floatingPointHours, floatingPointRate)
+if floatingPointHours > 40 :
+    # print("Overtime")
+    regular_pay = floatingPointHours * floatingPointRate
+    overtime_pay = (floatingPointHours - 40.0) * (floatingPointRate * 0.5)
+    # print(regular_pay,overtime_pay)
+    hours_times_rate = regular_pay + overtime_pay
+else :
+    # print("Regular")
+    regular_pay = floatingPointHours * floatingPointRate
+    hours_times_rate = floatingPointHours * floatingPointRate
+print("Pay:",hours_times_rate)
