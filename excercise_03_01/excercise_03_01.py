e2a2758004f3dd7851d67c31c8fb830bd057199b
nordtechsystems <tom.wilson@nordtechsystems.com>
2021-10-11 20:23:01 -0400

    Overtime Caluclator Complete -- excercise_03_01

--- excercise_03_01/excercise_03_01.py
+++ excercise_03_01/excercise_03_01.py
@@ -1,4 +1,16 @@
-xh = input("Enter Hours: ")
-xr = input("Enter Rate: ")
-xp = float(xh) * float(xr)
-print("Pay:",xp)
+enter_hours = input("Enter Hours: ")
+enter_rate = input("Enter Rate: ")
+floatingPointHours = float(enter_hours)
+floatingPointRate = float(enter_rate)
+# print(floatingPointHours, floatingPointRate)
+if floatingPointHours > 40 :
+    # print("Overtime")
+    regular_pay = floatingPointHours * floatingPointRate
+    overtime_pay = (floatingPointHours - 40.0) * (floatingPointRate * 0.5)
+    # print(regular_pay,overtime_pay)
+    hours_times_rate = regular_pay + overtime_pay
+else :
+    # print("Regular")
+    regular_pay = floatingPointHours * floatingPointRate
+    hours_times_rate = floatingPointHours * floatingPointRate
+print("Pay:",hours_times_rate)
