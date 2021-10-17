640f87b6c68a27021a2e69a5467cfdb9d046cc25
nordtechsystems <tom.wilson@nordtechsystems.com>
2021-10-17 16:59:22 -0400

    added _code_playground

---
+++ _code_playground/codInGame_1x1x1_Rubiks_cube_movements.py
@@ -0,0 +1,23 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+def main():
+
+    COUNTER_CLOCKWISE = "'"
+    LOCK_R_L = 'x'
+    LOCK_U_D = 'y'
+    # not used const LOCK_F_B = 'z';
+
+
+    rotations = input()
+    face_1 = input()
+    face_2 = input()
+
+    # Write an answer using print
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+    print("face1")
+    print("face2")
